51b21461945bf3f32a1880a2b2d967a0901e74e8
Abdullah <muhammadabdullahgohar572@gmail.com>
2025-02-10 02:09:15 -0800

    message

--- main.py
+++ main.py
@@ -1,11 +1,36 @@
+# age=20;
+# day="Wednesday"
+# price=12 if age>=18 else 8
+# if day=="Wednesday":
+#     price -= 2
+# print (f"Your Ticket Has been book and ${price}")
 
-age=20;
-day="Wednesday"
 
-price=12 if age>=18 else 8
+Number = 655
+Grade = ""
 
-if day=="Wednesday":
-    price -= 2
 
-print (f"Your Ticket Has been book and ${price}")
+if Number >=101:
+    print("Please cheak your number error is Number Wrong upload")
+    exit()
 
+if Number >= 90:
+    Grade = "A++"
+elif Number >= 80:
+    Grade = "A+"
+elif Number >= 70:
+    Grade = "A"
+elif Number >= 60:
+    Grade = "B"
+elif Number >= 50:
+    Grade = "C"
+elif Number >= 40:
+    Grade = "D"
+else:
+    Grade = "F"
+
+
+if Grade == "F":
+    print("Sorry to Saye but your Fail")
+else:
+    print(f" Your Are Passed And your Grade is{Grade}")

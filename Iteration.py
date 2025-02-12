f358dedd6efd8522949567fce9e2a934f277bbdb
Abdullah <muhammadabdullahgohar572@gmail.com>
2025-02-12 15:02:21 -0700

    test

---
+++ Iteration.py
@@ -0,0 +1,40 @@
+# myInformation={"name":"Abdullah","age":19,"city":"Karachi"}
+# print(myInformation["name"])
+
+# def square(number):
+#     return number*2
+
+
+# print(square(2))
+# print(square(4))
+# print(square(8))
+# import math
+# def calculate_distance(radius):
+#     area =math.pi* radius **2;
+#     cir=2*math.pi*radius
+#     return area,cir
+# a,b=calculate_distance(2)
+# print("Area:",a,":",end='')
+# print("Circumference:",b)
+
+
+# def name(name="Unknown"):
+#     if name=="Unknown":
+#         return "This is a Unknown user"
+#     else:
+#      return "This user name is ",name
+ 
+ 
+# print(name("Abdullah"))
+# print(name())
+
+# cude=lambda x:x**2
+
+# print(cude(2))
+
+
+# def value(*args):
+#    return sum(args)
+
+
+# print(value(1,2,5,78,9))
